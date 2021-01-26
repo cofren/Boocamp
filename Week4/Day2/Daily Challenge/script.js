97c72e9a23d29b2c4273567a43f79090cea9935f
cofren <a.gormanns@optamit.de>
2021-01-26 19:33:34 +0100

    update

--- Week4/Day2/Daily Challenge/script.js
+++ Week4/Day2/Daily Challenge/script.js
@@ -1,5 +1,23 @@
-// Daily Challenge "Stars"
+// Exercise Mini-Project
+let calcDisplay = document.getElementById('display')
+console.log(calcDisplay);
+let calcString = ("");
 
-let words = prompt("Please enter some words, separated by space");
-words = words.split(" ");
-console.log(words);
+// receive numbers and symbols
+
+function my_f(button) {
+    // console.log(button);
+    calcString = calcString + button;
+    calcDisplay.innerHTML = calcString;
+    console.log(calcString);
+}
+
+function result () {
+    let calcResult = eval(calcString);
+    console.log(calcResult);
+    calcDisplay.innerHTML = calcResult;
+}
+
+
+
+// hints: use new function called "results" when you click on the "=" sign in the calculator"
